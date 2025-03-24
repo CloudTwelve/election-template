5d7e2ece1b5b98961fa434009584a822c608e95c
CloudTwelve <94795982+CloudTwelve@users.noreply.github.com>
2025-03-24 16:23:25 +0000

    make and fill NavBar jsx, make Navbar css file

---
+++ election-website/src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = (props) => {
+    return (
+        <header>
+            <div class="title">
+                electioneer
+            </div>
+            <nav>
+                <button class="nav-select">
+                    Candidates
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Presidential</a>
+                    <a href="#">Treasurial</a>
+                    <a href="#">Secretarial</a>
+                </div>
+                <div class="non-drops">
+                    <a class="nav-select">
+                        Updates  
+                    </a>
+                    <a class="nav-select">
+                        Code of Conduct
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+};
