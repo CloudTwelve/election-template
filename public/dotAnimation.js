af5efa97e77b38322caad890a9d9f2380bcb9ea5
CloudTwelve <whippedreem@icloud.com>
2025-03-31 11:37:07 -0400

    add dot animation, add updates/codeOfConduct files css/js

---
+++ public/dotAnimation.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 75; i++)
+    {
+      let dot = document.createElement('div');
+      dot.classList.add("dot");
+      dot.style.position = "absolute";
+      dot.style.zIndex = -1;
+      dot.style.top = (Math.random() * 100) + '%';
+      dot.style.left = (Math.random() * 100) + '%';
+      dot.style.animation = "flying " + Math.floor(Math.random() * 20 + 20) + "s infinite ease"; 
+      dot.style.opacity = Math.floor(Math.random() * 100) + '%';
+      document.body.appendChild(dot);
+  }
+  
