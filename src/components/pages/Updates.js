6a8299b147c2ffe0582481151b43111f5d6def40
CloudTwelve <94795982+CloudTwelve@users.noreply.github.com>
2025-04-01 13:44:16 +0000

    add "under construction" to updates page

---
+++ src/components/pages/Updates.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Updates = () => {
+    return (
+        <div className="construct">
+            ** currently under construction **
+        </div>
+    );
+}
