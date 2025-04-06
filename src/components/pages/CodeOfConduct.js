8dde80a4421528922b60dc447ec02d3ce4a0c840
reemAhrabar <reemahrabar@gmail.com>
2025-04-06 03:39:32 -0400

    add code of conduct page + css

---
+++ src/components/pages/CodeOfConduct.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import './CodeOfConduct.css';
+
+const CodeOfConduct = (props) => {
+    return (
+    <main>
+    <Header />
+          <div class="italics">
+            <div class="intro">
+            <div class="small-title emphasis">
+            View our Policies:
+            </div>
+            <div class="arrow"></div>
+    </div>
+    <div class="cont">
+      <div class="summary">
+        In light of the senior class elections, we want to ensure a neutral stance and give logical insight into the election. Throughout reporting the election, we abide by the standards of conduct:
+      </div>
+      <div class="policy-block left">
+      Ensuring fact-based evidence and transparency
+      <ul>
+        <li>Posting relevant, recent, and unbiased posts by our authors</li>
+        </ul>
+    </div>
+       <div class="policy-block right">
+      Cultivating a secure environment to relay news
+      <ul>
+        <li>Maintaining a neutral and respectful tone in writing </li>
+        <li>Avoiding the use of emotionally charged language</li>
+        </ul>
+    </div>
+      <div class="policy-block left">
+      Presenting all candidates’ views
+      <ul>
+        <li>Ensuring all sides of the election are equally represented in light of controversies and disagreements</li>
+        </ul>
+    </div>
+      <div class="policy-block right">
+      Clarifying sources
+      <ul>
+        <li>Identifying and verifying all information before publishing</li>
+        <li>Certifying all quotes are attributed accurately</li>
+        </ul>
+    </div>
+      <div class="policy-block left">
+      Respecting privacy
+      <ul>
+        <li>Respecting all candidates’, especially of personal or sensitive information </li>
+        <li>Obtaining consent before reporting on personal matters</li>
+        <li>Delivering anonymous sources about identity confidentiality and trustworthiness</li>
+        </ul>
+    </div>
+      <div class="policy-block right">
+      Fostering diversity
+      <ul>
+        <li>Respecting diverse backgrounds, beliefs, and perspectives within the student body</li>
+        <li>Avoiding content that could alienate or offend others</li>
+        </ul>
+    </div>
+      </div>
+      <div class="policy-block left">
+      Avoiding sensationalizing
+      <ul>
+        <li>Focusing on facts</li>
+        <li>Refraining from exaggeration or sensationalizing stories for attraction</li>
+        </ul>
+    </div>
+    </div>
+    <div class="cont">
+      <div class="summary">
+        To ensure a safe, constructive, and respectful environment, we ask all 
+users to abide by the following: 
+      </div>
+      <div class="policy-block left">
+      Engaging in respectful communication
+      <ul>
+        <li>Treating all candidates and supporters with mutual respect</li>
+        <li>Disagreeing politely and avoiding personal attacks or inflammatory language</li>
+        <li>Recognizing that everyone is entitled to their own opinions</li>
+        </ul>
+    </div>
+       <div class="policy-block right">
+      Refraining digression
+      <ul>
+        <li>Keeping comments relevant to the candidates, election updates, and related topics 
+</li>
+        <li>Avoiding unrelated discussions or off-topic content</li>
+        <li>Offering ideas/solutions and providing constructive feedback</li>
+        </ul>
+    </div>
+    <div class="policy-block left">
+      Intolerance for discrimination or hate speech
+      <ul>
+        <li>Comments including hate speech, racism, ableism, sexism, or any other form of discrimination will not be tolerated</li>
+        <li>Fostering an inclusive environment where all can participate in conversation</li>
+        </ul>
+    </div>
+    <div class="policy-block right">
+      Maintaining privacy and confidentiality
+      <ul>
+        <li>Requiring consent in regards to the personal matters of candidates, fellow supporters, or students involved in the election process
+</li>
+        <li>Respecting the privacy of others, both on/offline</li>
+        </ul>
+    </div>
+    <div class="policy-block left">
+      Eradicating misinformation
+      <ul>
+        <li>Sharing factual information and avoiding spreading rumors about candidates</li>
+        <li>Ensuring all information is based in truth, and reality</li>
+        <li>Abstaining from targeting or discrediting other candidates</li>
+        <li>Maintain a fair tone in all discussions</li>
+        </ul>
+    </div>
+    <div class="policy-block right">
+      Reporting violations of the code
+      <ul>
+        <li>Disclose perceived violations to the moderators</li>
+        <li>Creating a safe and respectful environment for all participants</li>
+        </ul>
+    </div>
+    </div>
+  </main>
+    );
+};
+
+export default CodeOfConduct;
